de8126a38374ec3f192c5b8296bb9b1fd328bbb2
Andrew Moss <andrew@m0ss.dev>
2023-02-11 15:47:07 -0700

    chore: Working on docker config

---
+++ .changeset/dull-cows-thank.md
@@ -0,0 +1,8 @@
+---
+"eslint-config-m0ss": major
+"@m0ss/core": major
+"@m0ss/tsconfig": major
+"@m0ss/utils": major
+---
+
+First release
--- apps/m0ss/next.config.js
+++ apps/m0ss/next.config.js
@@ -1,9 +1,15 @@
+const path = require("path");
+
 module.exports = {
     reactStrictMode: true,
+    transpilePackages: ["@m0ss/core"],
     productionBrowserSourceMaps: false,
     images: {
         domains: ["m0ss.dev"],
         formats: ["image/avif", "image/webp"],
     },
     output: "standalone",
+    experimental: {
+        outputFileTracingRoot: path.join(__dirname, "../../"),
+      },
 };
