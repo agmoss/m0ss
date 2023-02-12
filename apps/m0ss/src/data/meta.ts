9c8a42ca36b4715f48c2b4f028812efccfe1ac58
Andrew Moss <andrew@m0ss.dev>
2023-02-11 17:16:24 -0700

    fix: Broken readme link

--- apps/m0ss/src/data/meta.ts
+++ apps/m0ss/src/data/meta.ts
@@ -7,7 +7,7 @@ export const metaData = {
     name: "Andrew Moss",
     logo: "https://m0ss.blob.core.windows.net/media/m0ss.png",
     readmeLink:
-        "https://raw.githubusercontent.com/agmoss/m0ss-v2/master/README.md",
+        "https://raw.githubusercontent.com/agmoss/m0ss/master/README.md",
     url: "https://m0ss.dev",
     homePage: {
         title: "Andrew Moss Website",
