af272071c765438a8bc7aa59ea1a9c1aa1e3799a
Andrew Moss <andrew@m0ss.dev>
2023-05-06 16:01:20 -0600

    chore: Package upgrade

--- apps/m0ss/src/pages/_document.tsx
+++ apps/m0ss/src/pages/_document.tsx
@@ -13,6 +13,14 @@ export default class _Document extends Document {
                         name="theme-color"
                         content={rawTheme.palette.primary.main}
                     />
+                <link
+                    rel="stylesheet"
+                    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+                />
+                <link
+                    rel="stylesheet"
+                    href="https://fonts.googleapis.com/css?family=Ubuntu+Mono:400,400i,700,700i&display=swap"
+                />
                 </Head>
                 <body>
                     <Main />
