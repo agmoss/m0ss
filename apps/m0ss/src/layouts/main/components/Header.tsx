505dc34449efd6efba78a789f1c1809c95dbc612
Andrew Moss <andrew@m0ss.dev>
2025-03-11 15:44:11 -0600

    chore: Slight upgrade

--- apps/m0ss/src/layouts/main/components/Header.tsx
+++ apps/m0ss/src/layouts/main/components/Header.tsx
@@ -120,7 +120,7 @@ export const Header = ({ themeMode, themeToggler }: IHeader) => {
                                             rx="2"
                                         />
                                         <polygon
-                                            fill="#333333"
+                                            fill="rgba(0, 0, 0, 0.54)"
                                             points="5.8 21.75 13.66 21.75 13.67 9.98 17.59 9.98 17.58 21.76 21.51 21.76 21.52 6.06 5.82 6.04 5.8 21.75"
                                         />
                                     </svg>
